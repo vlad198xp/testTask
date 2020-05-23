84450ce33ebd2d9cc1ba16b390494f54ed48bca5
Vladislav Gavrilenko <vlad198xp@gmail.com>
2020-05-23 13:14:04 +0300

    First commit

---
+++ testTask/testTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by Vladislav Gavrilenko on 5/23/20.
+//  Copyright © 2020 Vladislav Gavrilenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
